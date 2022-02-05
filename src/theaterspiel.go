aa23589d9a439ac68996ce2ce32db218df437a57
Enrico Speranza <enrico.speranza@gmail.com>
2022-02-05 23:47:27 +0100

    Update theaterspiel.go

--- src/theaterspiel.go
+++ src/theaterspiel.go
@@ -1,13 +1,27 @@
 package main
 
 import (
+	"fmt"
 	"strings"
+	"time"
 
 	"github.com/abiosoft/ishell/v2"
 	"moul.io/banner"
 )
 
-const VERSION = "Version 0.1 Alfa"
+const VERSION = "Version 0.2 Alfa"
+
+//https://go.dev/play/p/Q-Ufgrw3vZL
+
+func DDHHMMZ() string {
+	current_time := time.Now().UTC()
+	return fmt.Sprintf("%d%02d%02dZ\n", current_time.Day(), current_time.Hour(), current_time.Minute())
+}
+
+func DDHHMMZmmmYY() string {
+	current_time := time.Now().UTC()
+	return fmt.Sprintf(current_time.Format("021504ZJan06\n"))
+}
 
 func main() {
 	// create new shell.
@@ -24,6 +38,22 @@ func main() {
 		Help: "greet user",
 		Func: func(c *ishell.Context) {
 			c.Println("Hello", strings.Join(c.Args, " "))
+		},
+	})
+
+	shell.AddCmd(&ishell.Cmd{
+		Name: "datetime",
+		Help: "Zulu datetime",
+		Func: func(c *ishell.Context) {
+			c.Println("Zulu time:", DDHHMMZ())
+		},
+	})
+
+	shell.AddCmd(&ishell.Cmd{
+		Name: "ldatetime",
+		Help: "Zulu long datetime",
+		Func: func(c *ishell.Context) {
+			c.Println("Zulu time:", DDHHMMZmmmYY())
 		},
 	})
 
